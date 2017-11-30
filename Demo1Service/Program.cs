365978d0c18b3d0cda7b71fd4a7d6daa18a03db3
吴可 <470041973@qq.com>
2017-11-30 11:42:46 +0800

    测试项目代码管理

--- Demo1Service/Program.cs
+++ Demo1Service/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
         }
     }
 }
