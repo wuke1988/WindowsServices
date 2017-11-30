11e1efbcf6287b78df546017cec7a8a558ee9654
吴可 <470041973@qq.com>
2017-11-30 11:36:44 +0800

    添加项目文件。

---
+++ 0StoreDatabase/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0StoreDatabase
+{
+    [Serializable]
+    public class Product
+    {
+        public string ModelNumber
+        {
+            get;set;
+        }     
+        public string ModelName
+        {
+            get;set;
+        }
+       
+        public decimal UnitCost
+        {
+            get;set;
+        }
+
+        public string Description
+        {
+            get;set;
+        }
+      
+        public string CategoryName
+        {
+            get;set;
+        }
+
+        public int CategoryID
+        {
+            get;set;
+        }
+
+        public string ProductImagePath
+        {
+            get;set;
+        }
+    }
+}
---
+++ 0StoreDatabase/StoreDbDataSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0StoreDatabase
+{
+    public class StoreDbDataSet
+    {
+        internal static DataSet ReadDataSet()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXmlSchema("store.xsd");
+            ds.ReadXml("store.xml");
+            return ds;
+        }
+        public DataSet GetCategoriesAndProducts()
+        {
+            DataSet ds = ReadDataSet();
+            //DataRelation dataRelation = new DataRelation("CategoryProduct",ds.Tables["Categories"].Columns["CategoryID"],
+            //    ds.Tables["Products"].Columns["CategoryID"]);
+            //ds.Relations.Add(dataRelation);
+
+            return ds;
+        }
+        public DataTable GetProducts()
+        {
+            return ReadDataSet().Tables[0];
+        }
+    }
+}
