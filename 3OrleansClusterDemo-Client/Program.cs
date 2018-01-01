b81426853d4264d59b0e2260f43d7d7cc3ba50de
吴可 <470041973@qq.com>
2018-01-01 22:20:40 +0800

    完成Orleans入门实践：3. Silo集群部署及访问实践

---
+++ 3OrleansClusterDemo-Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using _2OrleansGrainStateDemo_Interface;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3OrleansClusterDemo_Client
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            InitialWithRetries();
+
+            Random random = new Random();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                var grainId = random.Next().ToString();
+
+                var grain = GrainClient.GrainFactory.GetGrain<IPersonGrain>("Test-"+grainId);
+
+                grain.SayHelloAsync();
+            }            
+        }
+        private static void InitialWithRetries()
+        {
+            try
+            {
+                GrainClient.Initialize("ClientConfiguration.config");
+                Console.WriteLine("Client success connect to silo");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed connect to silo {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Orleans/3OrleansClusterDemo-Silo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Orleans.Runtime.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3OrleansClusterDemo_Silo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ClusterSilo1";
+            try
+            {
+                using (var silo = new SiloHost(Console.Title))
+                {
+                    silo.ConfigFileName = "OrleansConfiguration.config";
+
+                    silo.LoadOrleansConfig();
+
+                    silo.InitializeOrleansSilo();
+
+                    silo.StartOrleansSilo();
+
+                    Console.WriteLine("Silo start successfully");
+                    Console.WriteLine("Press any key to exit");
+
+                    Console.ReadLine();
+
+                    silo.ShutdownOrleansSilo();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            Console.ReadLine();            
+        }
+    }
+}
