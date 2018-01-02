52f80b31ed5992d77b31ed7b96a74e51c150a8f2
吴可 <470041973@qq.com>
2018-01-02 18:24:47 +0800

    Orleans入门：4Stream添加新的客户端测试代码

---
+++ Orleans/4OrleansStreamDemo-ClientB/Program.cs
@@ -0,0 +1,44 @@
+﻿using _4OrleansStreamDemo;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4OrleansStreamDemo_ClientB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitialWithRetries();
+            while (true)
+            {               
+                var input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                var subscriberGrain = GrainClient.GrainFactory.GetGrain<IExplicitSubscriberGrain>(Guid.Empty);
+
+                var streamHandle = subscriberGrain.SubscribeAsync().Result;
+
+                Console.WriteLine("Press enter to exit...");
+                Console.ReadLine();
+
+                streamHandle.UnsubscribeAsync();
+            }
+        }
+        static void InitialWithRetries()
+        {
+            try
+            {
+                GrainClient.Initialize("ClientConfiguration.config");
+                Console.WriteLine("Client success connect to silo");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed connect to silo {ex.Message}");
+            }
+        }
+    }
+}
