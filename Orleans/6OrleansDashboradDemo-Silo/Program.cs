135fa3eced0af76f68062686659642efb72a45d5
吴可 <470041973@qq.com>
2018-01-06 17:26:46 +0800

    Orleans入门：6Dashbroad实践

---
+++ Orleans/6OrleansDashboradDemo-Silo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Orleans.Runtime.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6OrleansDashboradDemo_Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "DashboardSilo1";
+                using (var silo = new SiloHost("DashboardSilo1"))
+                {
+                    silo.ConfigFileName = "OrleansConfiguration.config";
+
+                    silo.LoadOrleansConfig();
+                    silo.InitializeOrleansSilo();
+                    silo.StartOrleansSilo();
+
+                    Console.WriteLine("Silo start successfully");
+                    Console.WriteLine("Press any key to exit");
+
+                    Console.ReadLine();
+                    silo.ShutdownOrleansSilo();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
